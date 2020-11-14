8c8e4f4a45f1d6302036468c2b6dd0c912488d5f
khansamad99 <samadk619@gmail.com>
2020-11-14 17:16:46 +0530

    Created axios and row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Row from './components/Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Row title="Netflix Originals"/>
+      <Row title="Trending Now"/>
     </div>
   );
 }
