265844d7deb7619e2fcdf037f962158fd67bfe8f
khansamad99 <samadk619@gmail.com>
2020-11-14 19:05:53 +0530

    Added Movie posters through API

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,17 +4,24 @@ import axios from '../axios';
 const Row = ({title,fetchURL}) => {
     const [movies,setMovies] = useState([]);
 
-    const fetchData = async () => {
-        const req = await axios.get(fetchURL);
-        console.log(req);
-    }
+    
+
     useEffect(() => {
+        const fetchData = async () => {
+            const req = await axios.get(fetchURL);
+            setMovies(req.data.results);
+        }
         fetchData();
-    },[]);
+    },[fetchURL]);
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+                <div className="row_posters">
+                    {movies.map(movie => (
+                        <img src={movie.poster_path} alt={movie.name}/>
+                    ))}
+                </div>
         </div>
     )
 }
