2b691ec95e1f374cd3e169e282cf78673599335e
khansamad99 <samadk619@gmail.com>
2020-11-14 18:32:15 +0530

    API called in row component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Row from './components/Row';
+import requests from './request';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Row title="Netflix Originals"/>
-      <Row title="Trending Now"/>
+      <Row title="Netflix Originals" fetchURL={requests.fetchNetflixOriginal}/>
+      <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
     </div>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import axios from '../axios';
 
-const Row = () => {
+const Row = ({title,fetchURL}) => {
+    const [movies,setMovies] = useState([]);
+
+    const fetchData = async () => {
+        const req = await axios.get(fetchURL);
+        console.log(req);
+    }
+    useEffect(() => {
+        fetchData();
+    },[]);
+
     return (
         <div>
-            
+            <h2>{title}</h2>
         </div>
     )
 }
