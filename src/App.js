ee7c8291ed0b083522e935bd86e0968829bc128b
khansamad99 <samadk619@gmail.com>
2020-11-15 01:37:32 +0530

    Added all categories

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Row title="Netflix Originals" fetchURL={requests.fetchNetflixOriginal}/>
-      <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+			<Row title="Trending" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
 			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
 			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
 			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
